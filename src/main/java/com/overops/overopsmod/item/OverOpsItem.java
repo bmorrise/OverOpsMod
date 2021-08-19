a514f4138bdf7d59b517a56f3b4341053f9b61ee
Benjamin Morrise <ben.morrise@overops.com>
2021-08-19 10:28:57 -0400

    Add additional block types

--- src/main/java/com/overops/overopsmod/item/OverOpsItem.java
+++ src/main/java/com/overops/overopsmod/item/OverOpsItem.java
@@ -12,7 +12,9 @@ import java.util.Set;
 import net.minecraft.block.Block;
 import net.minecraft.block.CactusBlock;
 import net.minecraft.block.DeadBushBlock;
+import net.minecraft.block.GrassBlock;
 import net.minecraft.block.SandBlock;
+import net.minecraft.block.TallGrassBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -35,8 +37,9 @@ public class OverOpsItem extends Item {
   private static Map<Class<? extends Block>, Number> possibleBlocks = new HashMap<>();
 
   static {
-    possibleBlocks.put(Block.class, 1);
-    possibleBlocks.put(DeadBushBlock.class, 1);
+    possibleBlocks.put(Block.class, 3);
+    possibleBlocks.put(TallGrassBlock.class, 2);
+    possibleBlocks.put(GrassBlock.class, 1);
     possibleBlocks.put(SandBlock.class, 1);
   }
 
@@ -44,15 +47,19 @@ public class OverOpsItem extends Item {
   public ActionResultType onItemUse(ItemUseContext context) {
     World world = context.getWorld();
 
-    if (!world.isRemote && world.getBlockState(context.getPos())
-        .getBlock() instanceof OverOpsBlock) {
-      world.destroyBlock(context.getPos(), false);
-    } else {
-      PlayerEntity playerEntity = Objects.requireNonNull(context.getPlayer());
-      Block block = world.getBlockState(context.getPos()).getBlock();
-      Number seconds = possibleBlocks.get(block.getClass());
+    if (!world.isRemote) {
+      if (world.getBlockState(context.getPos())
+          .getBlock() instanceof OverOpsBlock) {
+        world.destroyBlock(context.getPos(), false);
+      } else {
+        Block block = world.getBlockState(context.getPos()).getBlock();
 
-      playerEntity.setFire(seconds.intValue());
+        LOGGER.info("Using OverOpsItem on {}", block.getClass().toString());
+
+        PlayerEntity playerEntity = Objects.requireNonNull(context.getPlayer());
+        Number seconds = possibleBlocks.get(block.getClass());
+        playerEntity.setFire(seconds.intValue());
+      }
     }
 
     return super.onItemUse(context);
